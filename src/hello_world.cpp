c88f47fb682cd2db2fb122b69e5b0591683ece50
Wassim Akkari <was.akkari@gmail.com>
2024-09-23 19:44:14 +0100

    implement growing vec using geometric growth

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
+static void concat_vecs(std::vector<int>& v, std::vector<int>& newElems) {
+	const auto oldSize = v.size();
+	v.insert(
+		v.cend(),
+		std::move_iterator(newElems.begin()),
+		std::move_iterator(newElems.end())
+	);
+	//v.resize(v.size() + newElems.size());
+	// and
+	//std::copy(
+	//	std::move_iterator(newElems.begin()),
+	//	std::move_iterator(newElems.end()),
+	//	std::next(v.begin(), oldSize)
+	//);
+	// or
+	//std::transform(
+	//	std::move_iterator(newElems.begin()),
+	//	std::move_iterator(newElems.end()),
+	//	std::next(v.begin(), oldSize),
+	//	[](auto&& elem) { return elem * 2; }
+	//);
+}
+ 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector<int> list = { 1, 2, 3 };
+	std::vector<int> newElems = { 4, 6 };
+
+	concat_vecs(list, newElems);
+
+	for (const auto& element : list) {
+		std::cout << element << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "List capacity = " << list.capacity() << std::endl;
 }
