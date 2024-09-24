2114e76338c2c85196d29f848c131c408f41b445
Wassim Akkari <was.akkari@gmail.com>
2024-09-24 10:32:43 +0100

    get more infos from vftable->&RTTI (used for mumltiple inheritance).

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -15,6 +15,14 @@ public:
 };
 
 int main() {
-    Animal* animal = new Dog();
+    Dog d;
+    Animal* animal = &d;
     animal->speak(); // Outputs "Animal sound"
+    std::cout << typeid(animal).name() << std::endl; // this is uses the &RTTI field in the vftable
+    std::cout << typeid(*animal).name() << std::endl; // &RTTI: RunTime Type Information address which points to the class name
+
 }
+
+// The entire vftable and RTTI are stored in .rodata area of the program virtual memory
+// for more information refer to: https://github.com/navining/gocpp/blob/master/Chapter%206/Virtual-Functions-Static-Binding-and-Dynamic-Binding.md
+
