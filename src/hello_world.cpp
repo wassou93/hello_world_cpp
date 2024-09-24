07aca36aa364c5a47be3ba44caf8576a52386e41
Wassim Akkari <was.akkari@gmail.com>
2024-09-24 12:41:05 +0100

    difference between old c way and new iter way.

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Old way of iterating using pointers
+
+    int arr[] = { 10, 20, 30, 40 };
+
+    for (int* p = arr; p < arr + 4; ++p) {
+        std::cout << *p << '\n';
+    }
+
+    // New way using STL containers iterator
+
+    std::vector<int> v{ 10, 20, 30, 40, 50, 70 };
+
+    for (std::vector<int>::iterator pos = v.begin(); pos < v.end(); ++pos) {
+        std::cout << *pos << '\n';
+    }
 }
+ 
