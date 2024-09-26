d40dff3e6e80f9b2a0265349581f7b647b04ed13
Wassim Akkari <was.akkari@gmail.com>
2024-09-26 13:03:08 +0100

    Using a lambda expression

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 #include <set>
 
 struct Person 
@@ -6,15 +7,13 @@ struct Person
 	std::string name;
 	int age;
 };
-struct CompareByAge {
-	bool operator()(const Person& a, const Person& b) const {
-		return a.age < b.age;
-	}
-};
+
 
 int main()
 {
-	std::set<Person, CompareByAge> people = { { "Alice", 30 }, { "Bob", 25 }, { "Charlie", 35 } };
+	auto compare_by_age = [](const Person& person1, const Person& person2)-> bool {return person1.age < person2.age; };
+	std::set<Person, std::function<bool(const Person&, const Person&)>> people(compare_by_age);
+	people = {{"Alice", 30}, {"Bob", 25}, {"Charlie", 35}};
 
 	for (const auto& person : people)
 	{
@@ -23,4 +22,3 @@ int main()
 
 	return 0;
 }
-
