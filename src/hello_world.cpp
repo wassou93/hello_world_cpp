d1d2211516197bc3c84b28dcadc86ea7c689ccbc
Wassim Akkari <was.akkari@gmail.com>
2024-09-26 13:09:51 +0100

    a more consize way of using lambda as predicate without using functional header

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include <functional>
 #include <set>
 
-struct Person 
+struct Person
 {
-	std::string name;
-	int age;
+    std::string name;
+    int age;
 };
-
 
 int main()
 {
-	auto compare_by_age = [](const Person& person1, const Person& person2)-> bool {return person1.age < person2.age; };
-	std::set<Person, std::function<bool(const Person&, const Person&)>> people(compare_by_age);
-	people = {{"Alice", 30}, {"Bob", 25}, {"Charlie", 35}};
+    auto compare_by_age = [](const Person& person1, const Person& person2)-> bool {return person1.age < person2.age; };
+    std::set<Person, decltype(compare_by_age)> people(compare_by_age);
+    people = { {"Alice", 30}, {"Bob", 25}, {"Charlie", 35} };
 
-	for (const auto& person : people)
-	{
-		std::cout << person.name << " (" << person.age << " years old)\n";
-	}
+    for (const auto& person : people)
+    {
+        std::cout << person.name << " (" << person.age << " years old)\n";
+    }
+    
+    std::cout << typeid(compare_by_age).name() << std::endl;
 
-	return 0;
+    return 0;
 }
