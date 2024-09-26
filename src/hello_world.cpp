7aa7d33a79fa4371647c234caf447b74086a96fa
Wassim Akkari <was.akkari@gmail.com>
2024-09-26 10:07:57 +0100

    set comparison using operator<

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <set>
+
+struct Person 
+{
+	std::string name;
+	int age;
+
+	bool operator<(const Person& other) const
+	{
+		return age > other.age;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::set<Person> people = { {"Alice", 30}, {"Bob", 25}, {"Charlie", 35} };
+
+	for (const auto& person : people)
+	{
+		std::cout << person.name << " (" << person.age << " years old)\n";
+	}
+
+	return 0;
 }
+
