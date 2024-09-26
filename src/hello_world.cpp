4de652f9291b1b644409c71609e08c1cde5d4da3
Wassim Akkari <was.akkari@gmail.com>
2024-09-26 23:57:49 +0100

    Using tanges, views, pipes, takes, etc... (C++20 features)

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <ranges>
+#include <array>
+#include <numeric> 
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    array<int, 6> v{ 0 };
+    std::iota(v.begin(), v.end(), 0);
+    for (auto number : std::views::iota(0, 10) | std::views::take(5))
+    {
+        std::cout << number << " ";
+    }
+    std::cout << std::endl;
 }
